2108f68d3df3ac51c878cc934a14fc6272f2a0aa
rzyuser <1634565062@qq.com>
2021-10-24 11:08:31 +0800

    定义模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+#创建书籍信息
+class BookInfo(models.Model):
+    #id
+    name = models.CharField(max_length=10)
+
+#人物
+class PeopleInfo(models.Model):
+    name=models.CharField(max_length=10)
+    gender=models.BooleanField()
+    #外健约束:人物属于哪本书
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE())
