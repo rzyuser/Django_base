c8d7b865a74c3ac3b8201a08547be8627e6f608b
rzyuser <1634565062@qq.com>
2021-11-04 18:12:47 +0800

    视图03

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,7 +1,33 @@
 from django.urls import path
-from book.views import create_book,shop,register
+from book.views import create_book,shop,register,json,method,response
+
+from django.urls.converters import register_converter
+# 1.定义转化器
+class MobileConverter:
+    #验证数据的关键是: 正则
+
+    regex = '1[3-9]\d{9}'
+    #验证没有问题的数据,给视图函数
+    def to_python(self, value):
+        return value
+
+    def to_url(self, value):
+        #将匹配结果用于反解析传值时使用(了解)
+        return value
+#2.县注册转换器,才能在第三步中使用
+#converter  转换器的类
+#type_name 转换器的名字
+register_converter(MobileConverter,'phone')
+
 urlpatterns=[
     path('create/',create_book),
-    path('<city_id>/<shop_id>/',shop),
-    path('register/',register)
+    path('<int:city_id>/<phone:shop_id>/',shop),
+
+    #<转换器名字:变量名>
+    #转换器会对变量数据进行 正则的验证
+
+    path('register/',register),
+    path('json/',json),
+    path('method/',method),
+    path('res/',response),
 ]
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from book.models import BookInfo
+
 # Create your views here.
 def create_book(request):
     book=BookInfo.objects.create(
@@ -27,3 +28,40 @@ def register(requset):
     data=requset.POST
     print(data)
     return HttpResponse('ok')
+
+def json(request):
+
+    # request.POST  json数据不能通过request.POST获取数据
+    body=request.body
+    body_str=body.decode()
+    # print(body_str)
+
+    #JSON形式的字符串 可以转换为Python的字典
+    import json
+    body_dict=json.loads(body_str)
+    # print(body_dict)
+
+    ###########请求头###########
+    print(request.META['SERVER_PORT'])
+
+    return HttpResponse("json")
+
+
+def method(request):
+    print(request.method)
+    return HttpResponse('method')
+
+def response(request):
+    response = HttpResponse('res',status=200)
+    response['name'] = 'itcast'
+    return response
+    #1xx
+    #2xx
+    # 200 成功
+    #3xx
+    #4xx  请求有问题
+    #404  找不到页面  路由有问题
+    #403  403禁止访问  权限问题
+    #5xx
+    # 响应状态码100~599
+    return HttpResponse('response',status=404)
